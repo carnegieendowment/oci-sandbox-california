1092369dfc0779d12cb707efd3d500a8810db57e
sawojcicki <sawojcicki@gmail.com>
2017-01-31 14:05:08 -0500

    Update mapview.js

--- app/assets/scripts/views/mapview.js
+++ app/assets/scripts/views/mapview.js
@@ -77,7 +77,7 @@ var MapView = BaseView.extend({
       }
     )
     .addLayer(this.flaringLayers['2014'])
-    .setView([40, -220], 5);
+    .setView([40, -180], 5);
 
     new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
     this.map = map;
